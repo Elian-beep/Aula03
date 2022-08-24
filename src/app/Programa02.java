c8b9390ab81f407f7448673f80dfe2fe44dd1708
Elian-beep <elianoliver101@gmail.com>
2022-08-24 17:03:11 -0300

    programa03 sobre datas locais completado

--- src/app/Programa02.java
+++ src/app/Programa02.java
@@ -3,8 +3,11 @@ package app;
 public class Programa02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double a;
+		double b;
+		a = 5.0;
+		b = a;
+		System.out.println(b);
 	}
 
 }
--- src/app/Programa03.java
+++ src/app/Programa03.java
@@ -1,9 +1,39 @@
+// USANDO O DATA-HORA-LOCAL
+
 package app;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public class Programa03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Converter data-hora global para local
+		Instant d01 = Instant.parse("2022-08-23T01:30:06Z");
+		System.out.println("Horário Londres: " + d01);
+		LocalDateTime r1 = LocalDateTime.ofInstant(d01, ZoneId.systemDefault()); // Conversão acontecendo
+		System.out.println("Horário local: " + r1);
+
+		LocalDateTime r2 = LocalDateTime.ofInstant(d01, ZoneId.of("America/Sao_Paulo"));
+		System.out.println("Horário SP: " + r2);
+
+		LocalDateTime r3 = LocalDateTime.ofInstant(d01, ZoneId.of("Europe/Monaco"));
+		System.out.println("Horário Monaco: " + r3);
+
+//		Códigos de fuso horários
+//		for (String s : ZoneId.getAvailableZoneIds()) {
+//			System.out.println(s);
+//		}
+
+		// Obter dados de uma data-hora local
+		LocalDateTime d02 = LocalDateTime.parse("2022-08-23T01:30:06");
+		System.out.println("Dia: "+d02.getDayOfMonth());
+		System.out.println("Mês: "+d02.getMonth());
+		System.out.println("Ano: "+d02.getYear());
+		System.out.println("Hora: "+d02.getHour());
+		System.out.println("Minuto: "+d02.getMinute());
+		System.out.println(d02.getDayOfMonth()+"/"+d02.getMonth()+"/"+d02.getYear());
 
 	}
 
